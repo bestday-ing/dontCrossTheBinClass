801053a509c9fb3febf166b1e42175b678040c48
ko6154 <kc6154@naver.com>
2019-05-16 02:58:17 +0900

    csv파일에서 필요한 부분 추출
    교양 추출은 다시 구현필요

---
+++ csv_to_db.py
@@ -0,0 +1,47 @@
+import csv
+import pandas as pd
+from pandas import DataFrame
+
+
+'''
+전공과목 수정
+'''
+
+
+f_major = open('major.csv','r',encoding='utf-8')
+out_major = open('major_mod.csv', 'w', encoding='utf-8', newline='')
+rdr_m = csv.reader(f_major)
+wr_m = csv.writer(out_major)
+
+wr_m.writerow(['학년']+['전공']+['교과코드-구분']+['과목명']+['학점']+['담당교수']+['시간']+['강의실'])
+for line in rdr_m:
+    if(line[0]== '*'or line[0]=='1'or line[0]=='2' or line[0]=='3'or line[0]=='4'):
+     wr_m.writerow((line[0],line[1],line[2],line[3],line[5],line[8],line[9],line[12]))
+
+dataset = pd.read_csv('./major_mod.csv')
+print(dataset)      #전체 호출
+#print(dataset["과목명"])   #일부분 호출
+
+f_major.close()
+
+
+'''
+교양과목 수정
+'''
+f_liberal_arts = open('liberalarts.csv','r',encoding='utf-8')
+out_liberal_arts = open('lib_mod.csv', 'w', encoding='utf-8', newline='')
+
+rdr_l = csv.reader(f_liberal_arts)
+wr_l = csv.writer(out_liberal_arts)
+
+#wr_l.writerow(['전공']+['교과코드-구분']+['과목명']+['학점']+['담당교수']+['시간']+['강의실'])
+
+for line in rdr_l:
+    if(line[0]== '*'or line[0]=='1'or line[0]=='2' or line[0]=='3'or line[0]=='4'):
+     #wr_l.writerow((line[0],line[1],line[2],line[3],line[5],line[8],line[9],line[12]))
+     wr_l.writerow(line)
+
+#dataset2 = pd.read_csv('./lib_mod.csv')
+#print(dataset2)
+
+f_liberal_arts.close()
