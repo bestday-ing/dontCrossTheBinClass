0bca355fca6ab01e2de32c964cb6172e968ed5ec
idontknowusername <fiveshho@gmail.com>
2019-06-18 19:14:47 +0900

    컴퓨터학부 time index DB 구축
    일단 대충 눈으로 보기에 다 들어간거 같음 출력되는지 확인 필요

--- DB_setup.py
+++ DB_setup.py
@@ -1,23 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 import sqlite3
-import platform
-
-def CreateDataBase() :
-    DB.execute('DROP TABLE IF EXISTS Course;')
-    DB.execute('DROP TABLE IF EXISTS Time_INDEX;')
-    DB.execute('CREATE TABLE IF NOT EXISTS Course(code VARCHAR(15) PRIMARY KEY, type TEXT, \
-                year SMALLINT, cname TEXT, credit SMALLINT, pname TEXT, room TEXT, etc TEXT);')
-    DB.execute('CREATE TABLE IF NOT EXISTS Time_INDEX(time_id INTEGER PRIMARY KEY AUTOINCREMENT,\
-                code VARCHAR(15) REFERENCES Course(code), tindex int);')
-    con.commit()
 
 def ParsingTime(code, Time_ROW):
     if(Time_ROW == ''):
         return -1
     time_arr = list()
-    temp = Time_ROW.split('\n')
-    print(temp)
+    ##print(TT)
+    temp = Time_ROW.split(' ')
+    #print(temp)
     for j in range(len(temp)):  # 배열 길이만큼 j번 돌기
         if (temp[j][0] == '월'):
             col = 0
@@ -83,6 +72,9 @@ def ParsingTime(code, Time_ROW):
                 row = returnROW(k)
                 time = row * 7 + col
                 time_arr.append(time)
+    #time_str = time_str.rstrip(',')  # 마지막에 콤마(,) 하나 떼기
+    #print(time_arr)  # 음수는 0교시 혹은 다른 예외의 경우
+    #print(code)
     INSERT_TIME_INDEX(code, time_arr)
     return
 def returnROW(single_time):
@@ -145,7 +137,7 @@ def returnROW(single_time):
     return row
 
 def INSERT(tuple = []) :
-    DB.execute("INSERT OR IGNORE INTO Course values(?, ?, ?, ?, ?, ?, ?, ?);", tuple)
+    DB.execute("INSERT OR IGNORE INTO Course values(?, ?, ?, ?, ?, ?);", tuple)
     con.commit()
 
 def INSERT_TIME_INDEX(code, ctime = []) :
@@ -155,74 +147,42 @@ def INSERT_TIME_INDEX(code, ctime = []) :
     con.commit()
 
 def SELECT_db():
-    for row in DB.execute("SELECT * FROM Course"):
+    for row in DB.execute("SELECT * FROM Time_INDEX WHERE time_id > 1500"):
         print(row)
-# 크롬 드라이버 및 데이터베이스 셋업 #
-windowDriverPath = 'C:/Python/chromedriver'
-chrome_option = webdriver.ChromeOptions()
-chrome_option.add_argument("--window-size=1280,720")  # 윈도우 사이즈 조절해서 모든 column 로드
-#chrome_option.add_argument("headless")                # 창 없는 크롬 모드
-if(platform.system() == 'Windows'): #platform
-    driver = webdriver.Chrome(windowDriverPath, options=chrome_option)  # 윈도우 크롬 드라이버 불러오기
-    driver.implicitly_wait(3)
+
+def CreateDataBase() :
+    DB.execute('DROP TABLE IF EXISTS Course;')
+    DB.execute('DROP TABLE IF EXISTS Time_INDEX;')
+    DB.execute('CREATE TABLE IF NOT EXISTS Course(code VARCHAR(15) PRIMARY KEY, type TEXT, \
+                year SMALLINT, cname TEXT, credit SMALLINT, pname TEXT);')
+    DB.execute('CREATE TABLE IF NOT EXISTS Time_INDEX(time_id INTEGER PRIMARY KEY AUTOINCREMENT,\
+                code VARCHAR(15) REFERENCES Course(code), tindex int);')
+    con.commit()
+# 데이터베이스 셋업 #
 DB_Path = "./Project.db"
 con = sqlite3.connect(DB_Path)
 DB = con.cursor()
-#######################################
-
-def select_Search_by_ID(id, find):  # id : html id 태그, find : 타겟
-    select = Select(driver.find_element_by_id(id))
-    select.select_by_visible_text(find)
-
-def crawled():
-    cTable = driver.find_elements_by_tag_name('tr')
-    TupleClass = {'code' : 'th4', 'gubun' : 'th2', 'year' : 'th1', 'cname' : 'th5',
-                 'credit' : 'th6', 'pname' : 'th9', 'room' : 'th11' ,'etc' : 'th16'}
-    Tuple = list()  # 데이터베이스에 insert 될 튜플
-
-    for Table_row in cTable:
-        if(Table_row == cTable[0]):
-            continue
-        for classname in TupleClass.values():
-            Tuple.append(Table_row.find_element_by_class_name(classname).text)
-        if(Tuple[2] == '*'):
-            Tuple[2] = 0
-        Tuple[2] = int(Tuple[2])
-        Tuple[4] = int(Tuple[4])
-        INSERT(Tuple)
-        ParsingTime(Tuple[0], Table_row.find_element_by_class_name('th10').text)
-
-        Tuple.clear()
-
+######################
 CreateDataBase()
-driver.get('http://my.knu.ac.kr/stpo/stpo/cour/listLectPln/list.action')
-select_Search_by_ID('mainDiv', '대학')
 
-cat1 = driver.find_element_by_id('sub02')
-cat1 = cat1.find_elements_by_tag_name('option')
+import csv
+import pandas as pd
 
-all_major = [[0 for i in range(0)] for j in range(21)]
+major = open('major_mod.csv','r',encoding='utf-8')
+rdr_m = csv.reader(major)
+TUPLE = list();
+for line in rdr_m:
+    if(line[0] == '*'):
+        line[0] = line[0].replace('*','0')        # 학년구분 없음: 0으로 casting
+    TUPLE.append(line[2])           # 과목코드
+    TUPLE.append(line[1])           # 구분
+    TUPLE.append(int(line[0]))      # 학년
+    TUPLE.append(line[3])           # 과목명
+    TUPLE.append(int(line[4]))      # 학점
+    TUPLE.append(line[5])           # 담당교수
+    print(TUPLE)
+    INSERT(TUPLE)                   # Course 테이블에 넣기
+    ParsingTime(TUPLE[0], line[6])  # Time_INDEX 테이블 구성
+    TUPLE.clear()
 
-for i in range(len(cat1)):
-    dept = cat1[i]
-    if(dept.text == '==상주캠퍼스=='):
-        break
-    #print('=====' + dept.text + '=====')
-    select_Search_by_ID('sub02', dept.text)     # sub02 id값 dept.text로 선택
-    all_major[i].append(dept.text)
-
-    cat2 = driver.find_element_by_id('sub2')
-    cat2 = cat2.find_elements_by_tag_name('option')
-    for major in cat2:
-        #print('---' + major.text)
-        select_Search_by_ID('sub2', major.text)
-        if(major.text == '컴퓨터학부' or major.text == '컴퓨터학부 글로벌소프트웨어융합전공'):
-             continue
-        all_major[i].append(major.text)
-
-print(all_major)
-        # driver.find_element_by_id('doSearch').click()
-        # crawled()
-
-# SELECT_db()
-driver.close()
+SELECT_db()
--- csv_to_db.py
+++ csv_to_db.py
@@ -12,19 +12,21 @@ from pandas import DataFrame
 # .drop등 명령어로 특정부분 삭제 가능
 
 
-f_major = open('major.csv','r',encoding='utf-8')                            #Input 전공 파일 이름
+f_major = open('computer.csv','r',encoding='utf-8')                            #Input 전공 파일 이름
 out_major = open('major_mod.csv', 'w', encoding='utf-8' ,newline='')        #Output 전공 파일 이름
 rdr_m = csv.reader(f_major)
 wr_m = csv.writer(out_major)
 
-wr_m.writerow(['학년']+['전공']+['교과코드-구분']+['과목명']+['학점']+['담당교수']+['시간'])
+#wr_m.writerow(['학년']+['전공']+['교과코드-구분']+['과목명']+['학점']+['담당교수']+['시간'])
 
 for line in rdr_m:
+    print(line)
     if(line[0]== '*'or line[0]=='1'or line[0]=='2' or line[0]=='3'or line[0]=='4'):
-     wr_m.writerow((line[0],line[1],line[2],line[3],line[5],line[8],line[9]))
+        line[2] = line[2].replace('-','')
+        wr_m.writerow((line[0],line[1],line[2],line[3],line[5],line[8],line[9]))
 
 dataset = pd.read_csv('./major_mod.csv')
-print(dataset)      #전체 호출
+#print(dataset)      #전체 호출
 #print(dataset["과목명"])   #일부분 호출
 
 f_major.close()
@@ -42,13 +44,14 @@ out_liberal_arts = open('lib_mod.csv', 'w', encoding='utf-8', newline='')      #
 rdr_l = csv.reader(f_liberal_arts)
 wr_l = csv.writer(out_liberal_arts)
 
-wr_l.writerow(['학년']+['전공']+['교과코드-구분']+['과목명']+['학점']+['담당교수']+['시간']+['강좌관리과'])
+wr_l.writerow(['학년']+['전공']+['교과코드-구분']+['과목명']+['학점']+['담당교수']+['시간'])#+['강좌관리과'])
 
 for line in rdr_l:
     if(not line[0] and not line[1] and line[2]):
         line[0]='*'
         line[1]='교양'
-        wr_l.writerow((line[0],line[1],line[2],line[3],line[4],line[8],line[9],line[13]))
+        line[2] = line[2].replace('-','')
+        wr_l.writerow((line[0],line[1],line[2],line[3],line[4],line[8],line[9]))
 
 #dataset2 = pd.read_csv('./lib_mod.csv',sep =',')
 #print(dataset2)
